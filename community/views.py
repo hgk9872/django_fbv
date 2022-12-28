f4dc0ebd0fcf7718f73202ca1b635e7c79777342
hugo <hgk9872@gmail.com>
2022-12-29 01:47:40 +0900

    post_create form 추가

--- community/views.py
+++ community/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
 
 
 def index(request):
@@ -18,3 +19,16 @@ def comment_create(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     post.comment_set.create(content=request.POST.get('content'))
     return redirect('community:detail', post_id=post.id)
+
+
+def post_create(request):
+    # 글 작성을 위해 POST 요청
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('community:index')
+    else: # GET 요청
+        form = PostForm() # empty form
+    context = {'form': form}
+    return render(request, 'community/post_form.html', context)
